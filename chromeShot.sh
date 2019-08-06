3e55951aee2f1681cc0c85016c0efd5b727471ab
anandsreekumaras <anandsree97@gmail.com>
2019-08-06 22:48:44 +0530

    first version

---
+++ chromeShot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ChromeShot is tool to take screenshot of URLs or IPs from a text file using google-chrome --headless method
+# written by Anand A S https://anandsreekumar.com
+filename='hosts.txt' 
+filelines=`cat $filename`
+dir="output"
+directory=$filename-$dir
+mkdir $directory
+for line in $filelines ; do
+	export HOST=$line
+	google-chrome-stable --headless --window-size=1024,768 --hide-scrollbars --screenshot=$directory/${HOST}.png http://${HOST}
+	echo Saved $directory/${HOST}.png
+done
+echo Finished $filename
